5f21aaa304f21414a0b1aa7680027af447e2ee38
Yudai Nakajima <kituneudon97@gmail.com>
2024-06-22 17:52:44 +0900

    #4 リクエストとレスポンスのスキーマを定義

--- api/src/routers/task.py
+++ api/src/routers/task.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from src.schemas.task import TaskCreateResponse
+from src.schemas.task import TaskCreateResponse, TaskCreateRequest
 
 router = APIRouter()
 
-@router.post("/tasks")
-async def create_task(task_body: TaskCreateResponse):
-    return {"message": "Task created successfully"}
+@router.post("/tasks", response_model=TaskCreateResponse, tags=["tasks"])
+async def create_task(task_body: TaskCreateRequest):
+    return {"result": "Task created successfully"}
--- api/src/schemas/task.py
+++ api/src/schemas/task.py
@@ -2,5 +2,8 @@ from pydantic import BaseModel, Field
 from datetime import datetime
 
 class TaskCreateResponse(BaseModel):
+    result: str = Field(..., example="Task created successfully")
+
+class TaskCreateRequest(BaseModel):
     title: str = Field(..., example="クリーニングに取りに行く")
     description: str | None = Field(None, example="〇〇クリーニングに取りに行く")
