2dd3007466088a8eb6405563571fbc2ca5f7ec0c
nakajima97 <kituneudon97@gmail.com>
2024-06-25 08:43:17 +0900

    #4 DBに値が入ることを確認

--- api/src/routers/task.py
+++ api/src/routers/task.py
@@ -1,9 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
+from src.db import get_db
 from src.schemas.task import TaskCreateResponse, TaskCreateRequest
+from src.models.task import Task
+from src.repositories.task_repository import TaskRepository
+from src.usecases.create_task import CreateTask
 
 router = APIRouter()
 
 @router.post("/tasks", response_model=TaskCreateResponse, tags=["tasks"])
-async def create_task(task_body: TaskCreateRequest):
+async def create_task(task_body: TaskCreateRequest, db: Session = Depends(get_db)):
+    task = Task(**task_body.dict())
+
+    task_repository = TaskRepository(db)
+    CreateTask(task_repository).exec(task)
+
     return {"result": "Task created successfully"}
