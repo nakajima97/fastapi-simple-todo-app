ec793cae8ca32fa9a0342d613219dd97005f4adb
Yudai Nakajima <kituneudon97@gmail.com>
2024-06-29 17:45:45 +0900

    #14 DBに値がない場合のテストコードを作成

--- api/test/features/task/test_get_task.py
+++ api/test/features/task/test_get_task.py
@@ -6,3 +6,4 @@ async def test_get_task_empty(async_client: AsyncSession):
     response = await async_client.get("/tasks")
 
     assert response.status_code == 200
+    assert response.json() == {"tasks": []}
