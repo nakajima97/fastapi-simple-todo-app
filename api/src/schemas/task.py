6c7c8f9dc94cf68d4c1f3f9605041fe0d7f7fb9b
Nakajima <kituneudon97@gmail.com>
2024-06-29 17:12:06 +0900

    POST /tasks の機能テスト作成 (#17)
    * #15 動かないけど一旦コミット。メソッドを非同期にする必要ある。
    
    * #15 warnの解消
    
    * #15 最小構成の機能テストを作成
    
    * #15 フォーマッターの実行
    
    * #15 メッセージの形式を確認
    
    * #15 warning修正
    
    * #15 フォーマッターの実行

--- api/src/schemas/task.py
+++ api/src/schemas/task.py
@@ -2,9 +2,22 @@ from pydantic import BaseModel, Field
 
 
 class TaskCreateResponse(BaseModel):
-    result: str = Field(..., example="Task created successfully")
+    result: str = Field(...)
+
+    model_config = {
+        "json_schema_extra": {"example": {"result": "Task created successfully"}}
+    }
 
 
 class TaskCreateRequest(BaseModel):
-    title: str = Field(..., example="クリーニングに取りに行く")
-    description: str | None = Field(None, example="〇〇クリーニングに取りに行く")
+    title: str = Field(...)
+    description: str | None = Field(None)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "クリーニングに取りに行く",
+                "description": "〇〇クリーニングに取りに行く",
+            }
+        }
+    }
