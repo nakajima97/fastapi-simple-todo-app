4fd3961c069aef5cb5dcb7f0de2ce897f1161bd2
nakajima97 <kituneudon97@gmail.com>
2024-07-09 21:45:39 +0900

    #23 すでに完了済みのタスクを完了にしたときのテストコード作成

--- api/tests/features/task/test_finish_task.py
+++ api/tests/features/task/test_finish_task.py
@@ -18,3 +18,13 @@ async def test_finish_task_when_task_not_found(async_client: AsyncSession):
     response = await async_client.put("/tasks/1/finish")
     assert response.status_code == 200
     assert response.json() == {"result": "Task not found"}
+
+@pytest.mark.asyncio
+async def test_finish_task_when_task_already_finished(async_client: AsyncSession, dummy_tasks):
+    '''
+    既に完了しているタスクを再度完了させようとしたときのテスト
+    '''
+    await async_client.put("/tasks/2/finish")
+    response = await async_client.put("/tasks/2/finish")
+    assert response.status_code == 400
+    assert response.json() == {"result": "Task finished successfully"}
