e49c02541a5540edd646eb30b17f3c34d2c7031e
nakajima97 <kituneudon97@gmail.com>
2024-07-09 21:46:34 +0900

    #23 フォーマッターの実行

--- api/tests/features/task/test_finish_task.py
+++ api/tests/features/task/test_finish_task.py
@@ -1,29 +1,34 @@
 import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
 
+
 @pytest.mark.asyncio
 async def test_finish_task(async_client: AsyncSession, dummy_tasks):
-    '''
+    """
     タスクを完了したときのテスト
-    '''
+    """
     response = await async_client.put("/tasks/1/finish")
     assert response.status_code == 200
     assert response.json() == {"result": "Task finished successfully"}
 
+
 @pytest.mark.asyncio
 async def test_finish_task_when_task_not_found(async_client: AsyncSession):
-    '''
+    """
     タスクが見つからないときのテスト
-    '''
+    """
     response = await async_client.put("/tasks/1/finish")
     assert response.status_code == 200
     assert response.json() == {"result": "Task not found"}
 
+
 @pytest.mark.asyncio
-async def test_finish_task_when_task_already_finished(async_client: AsyncSession, dummy_tasks):
-    '''
+async def test_finish_task_when_task_already_finished(
+    async_client: AsyncSession, dummy_tasks
+):
+    """
     既に完了しているタスクを再度完了させようとしたときのテスト
-    '''
+    """
     await async_client.put("/tasks/2/finish")
     response = await async_client.put("/tasks/2/finish")
     assert response.status_code == 400
