3b3df91d39cd402437b26a5af0a6f985acc0d0ae
nakajima97 <kituneudon97@gmail.com>
2024-07-09 21:42:08 +0900

    #23 存在しないタスクを完了したときのテストコードを追加

--- api/tests/features/task/test_finish_task.py
+++ api/tests/features/task/test_finish_task.py
@@ -3,6 +3,18 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 @pytest.mark.asyncio
 async def test_finish_task(async_client: AsyncSession, dummy_tasks):
+    '''
+    タスクを完了したときのテスト
+    '''
     response = await async_client.put("/tasks/1/finish")
     assert response.status_code == 200
     assert response.json() == {"result": "Task finished successfully"}
+
+@pytest.mark.asyncio
+async def test_finish_task_when_task_not_found(async_client: AsyncSession):
+    '''
+    タスクが見つからないときのテスト
+    '''
+    response = await async_client.put("/tasks/1/finish")
+    assert response.status_code == 200
+    assert response.json() == {"result": "Task not found"}
